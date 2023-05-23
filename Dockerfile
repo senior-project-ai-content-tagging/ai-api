baa74892a4a78f88f89d4d9aab61dfc2138184f0
thekogo <thekogo@gmail.com>
2023-05-24 01:11:06 +0700

    add automate deploy after push

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM python:3.8-slim-buster
 
-RUN apt-get update && apt-get install -y libpq-dev build-essential default-libmysqlclient-dev libpq5
+ENV MODEL_URL="https://storage.googleapis.com/ai-modal/ai-modal.joblib"
+ENV TFIDF_URL="https://drive.google.com/uc?export=download&id=1FN63hj5A_Y28TUBgZ6nn8oo--_85ywE7"
+
+RUN apt-get update && apt-get install -y libpq-dev build-essential default-libmysqlclient-dev libpq5 wget
 
 
 WORKDIR /app
@@ -17,6 +20,11 @@ ENV NLTK_DATA=/usr/share/nltk_data
 RUN mkdir -p $NLTK_DATA
 COPY download_nltk_data.py .
 RUN python download_nltk_data.py
+
+# donwload model (large file)
+RUN pip install gdown
+RUN gdown "$MODEL_URL" -O model.joblib
+RUN gdown "$TFIDF_URL" -O tfidf.pkl
 
 COPY . /app/
 
--- server.py
+++ server.py
@@ -11,7 +11,7 @@ from utils.data_processing import preprocessing
 PREPARED_CONTENT_SUBSCRIPTION = 'projects/senior-project-364818/subscriptions/content-preparing.prepared-content-sub'
 
 FIND_ID_AND_CONTENT_EN_BY_TICKET_ID_SQL = """
-SELECT contents.id, content_en FROM contents
+SELECT contents.id, content_en, title_en FROM contents
 JOIN tickets ON contents.id = tickets.content_id
 WHERE tickets.id = %s
 """
@@ -31,39 +31,63 @@ UPDATE tickets SET status = %s
 WHERE id = %s
 """
 
-conn_str = f'host={os.environ.get("DB_HOST")} user={os.environ.get("DB_USER")} password={os.environ.get("DB_PASS")} dbname={os.environ.get("DB_NAME")} options=project=delicate-hat-661219 sslmode=require'
-conn = psycopg2.connect(conn_str)
-
 # Step 2: Load saved model
 # with open('knn_eng_tune.sav', 'rb') as f:
 # my_model = pickle.load(open('./knn_eng_tune.sav', 'rb'))
 # my_model = load_model('./model_lstm_eng.h5')
-my_model = joblib.load('./random_forest_eng_tune.joblib')
+my_model = joblib.load('./model.joblib')
 
-tfidf_eng, train_data = pickle.load(open('./tfidf_eng.pkl', 'rb'))
+tfidf_eng, train_data = pickle.load(open('./tfidf.pkl', 'rb'))
 
 app = Flask(__name__)
 
 
 def callback(json_message):
-    ticket_id = json_message["ticketId"]
+    # Establish database connection
+    conn_str = f'host={os.environ.get("DB_HOST")} user={os.environ.get("DB_USER")} password={os.environ.get("DB_PASS")} dbname={os.environ.get("DB_NAME")} options=project=delicate-hat-661219 sslmode=require'
+    conn = psycopg2.connect(conn_str)
 
-    cursor = conn.cursor()
-    cursor.execute(FIND_ID_AND_CONTENT_EN_BY_TICKET_ID_SQL, (str(ticket_id), ))
-    (content_id, content_en) = cursor.fetchone()
-    preprocessing_data = preprocessing(content_en)
-    print(preprocessing_data)
-    vector_text = tfidf_eng.transform([preprocessing_data])
-    result = my_model.predict(vector_text)
-    for category in result:
-        print(category)
-        cursor.execute(FIND_CATEGORY_ID_BY_NAME_SQL, (category, ))
-        category_id = cursor.fetchone()[0]
-        cursor.execute(CREATE_CONTENT_CATEGORY_SQL, (str(content_id), str(category_id)))
-        cursor.execute(UPDATE_TICKET_STATUS_SQL, ("DONE", str(ticket_id)))
+    try:
+        ticket_id = json_message["ticketId"]
 
-    conn.commit()
-    cursor.close()
+        # Fetch content ID and content in English
+        with conn.cursor() as cursor:
+            cursor.execute(FIND_ID_AND_CONTENT_EN_BY_TICKET_ID_SQL, (str(ticket_id), ))
+            content_id, content_en, title_en = cursor.fetchone()
+            title_en = "" if title_en is None else title_en
+
+            # Preprocess content
+            preprocessing_data = preprocessing(title_en + content_en)
+            print(preprocessing_data)
+
+            # Transform preprocessed data using TF-IDF
+            vector_text = tfidf_eng.transform([preprocessing_data])
+
+            # Predict categories
+            result = my_model.predict(vector_text)
+
+            for category in result:
+                print(category)
+                # Find category ID
+                cursor.execute(FIND_CATEGORY_ID_BY_NAME_SQL, (category, ))
+                category_id = cursor.fetchone()[0]
+
+                # Create content category
+                cursor.execute(CREATE_CONTENT_CATEGORY_SQL, (str(content_id), str(category_id)))
+
+            # Update ticket status to "DONE"
+            cursor.execute(UPDATE_TICKET_STATUS_SQL, ("DONE", str(ticket_id)))
+
+        # Commit the changes
+        conn.commit()
+
+    except (KeyError, psycopg2.Error) as e:
+        # Handle exceptions and errors
+        print(f"An error occurred: {e}")
+
+    finally:
+        # Close the cursor and connection
+        conn.close()
 
 
 @app.route("/predict", methods=["POST"])
